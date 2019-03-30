107ef89a85ccc361a4187d9acb5f798fd4e67cb4
zl <871829440@qq.com>
2019-03-30 14:06:46 +0800

    first push

---
+++ src/zhulei/JavaNote/阻塞队列/生产者消费者模型/Producer.java
@@ -0,0 +1,15 @@
+package 朱磊.JavaNote.阻塞队列.生产者消费者模型;
+
+/**
+ * @Author: zl
+ * @Date: 2019/3/30 13:28
+ * @Description:
+ */
+public class Producer implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+}
